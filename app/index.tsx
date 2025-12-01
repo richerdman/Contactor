c32287ce0d7584e0a69191eb81ec3e3a8548c964
richerdman <112634685+richerdman@users.noreply.github.com>
2025-12-01 09:53:30 +0000

    default screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,38 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+import { COLORS, FONT_SIZES, SPACING } from "../src/constants/theme";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+
+export default function HomeScreen() {
+	  {/* Router instance to navigate between screens: const router = useRouter(); */}
+    
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Contacts</Text>
+            <Text style={styles.bottomText}>Add Contact</Text>
+            {/* button router.push to add contact details*/}
+        </View>
+    );
 }
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: COLORS.background,
+            justifyContent: "center",
+            alignItems: "center",
+            padding: SPACING.lg,
+        },
+        title: {
+            fontSize: FONT_SIZES.xlarge,
+            fontWeight: "bold",
+            color: COLORS.textPrimary,
+            marginBottom: SPACING.sm,
+        },
+        bottomText: {
+            fontSize: FONT_SIZES.medium,
+            color: COLORS.primary,
+            marginTop: SPACING.md,
+        },
+});
