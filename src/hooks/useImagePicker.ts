78e20ce861eb9e6436fdfedc1ae012e5a7ead2a9
olafurd24 <80835762+OliDada@users.noreply.github.com>
2025-12-04 10:29:16 +0000

    Add image with camera or photo import

---
+++ src/hooks/useImagePicker.ts
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+
+export function useImagePicker() {
+    const [image, setImage] = useState<string | null>(null);
+
+    const pickImage = async () => {
+        // Request permission to access media library
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+            alert('Sorry, we need media library permissions to make this work!');
+            return;
+        }
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    };
+
+    const takePhoto = async () => {
+        // Request permission to access camera
+        const { status } = await ImagePicker.requestCameraPermissionsAsync();
+        if (status !== 'granted') {
+            alert('Sorry, we need camera permissions to make this work!');
+            return;
+        }
+        let result = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    };
+
+    return { image, pickImage, takePhoto };
+}
